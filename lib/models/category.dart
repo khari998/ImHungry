991386f1ef7d1fc02668e5fab83a488af54db7b0
Khari <khari.lane@khalikenterprises.com>
2019-07-25 16:40:20 -0500

    Made Category model and Category dummy data

---
+++ lib/models/category.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id; // give unique id to each Category
+  final String title;
+  final Color color; // provided through material.dart
+  const Category({ // Category onstructor with named arguments
+    @required this.id, // require id property
+    @required this.title, // require title property
+    this.color = Colors.red, // provide default color through constructor
+  });
+}
