a314d94720189d29323c25274f6ddd450bc5faf7
Khari <khari.lane@khalikenterprises.com>
2019-07-25 17:16:37 -0500

    Scrollable grid of mapped Category widgets rendering on home page

--- lib/food_cats.dart
+++ lib/food_cats.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import './dummy-data.dart';
+import './category_item.dart';
 
 class FoodCategories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2 ,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Food Categories"), // compile time constant title
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((cat) => CategoryItem(
+                  // map the dummy data into Category Widgets
+                  cat.title,
+                  cat.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './food_cats.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,26 +11,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: FoodCategories(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget { 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Food Choices'),
-      ),
-      body: Center(child: Text('Centered Text'),),
-    );
-  }
-}
